8a4691254f39bf469f0fa81df11fa529bca93eec
monowar-hossain.github.io <32286368+monowar-hossain@users.noreply.github.com>
2017-09-28 18:34:36 +0600

    class-7

---
+++ class-7/assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+		
+		$(".staff-list").owlCarousel({
+			items:4,
+			margin:20,
+			loop:true,
+			nav:true,
+			tavText:["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"]
+			
+		})
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
