d4658780663a18d172c5f03f8c38555e12950baf
monowar-hossain.github.io <32286368+monowar-hossain@users.noreply.github.com>
2017-10-05 15:43:54 +0600

    class-15

---
+++ class-15/assets/js/main.js
@@ -0,0 +1,42 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+		$(".product-list").masonry();
+		
+		$('.homepage-slides').owlCarousel({
+			items:1,
+			loop:true,
+			autoplay:true,
+			dots:false,
+			autoplaySpeed:1500,
+			nav:true,
+			navText:["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"]
+			
+		})
+		$('.product-promotion').owlCarousel({
+			items:1,
+			loop:true,
+			autoplay:false,
+			autoplaySpeed:1000,
+			nav:false,
+			
+		})
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
