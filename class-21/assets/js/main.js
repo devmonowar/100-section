4e17d5d42ab70ed5a34938f6ca407c642279db44
monowar-hossain.github.io <32286368+monowar-hossain@users.noreply.github.com>
2017-10-08 18:50:27 +0600

    class-21

---
+++ class-21/assets/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+	$(".homepage-slides").owlCarousel({
+		items:1,
+		loop:true,
+		margin:30,
+		nav:false,
+		dots:true
+	})
+        
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
