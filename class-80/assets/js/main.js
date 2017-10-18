9c24800edf905dbd4c3194fb68e293b70902b8a5
monowar-hossain <32286368+monowar-hossain@users.noreply.github.com>
2017-10-18 09:44:12 +0600

    fgesrg

---
+++ class-80/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+		
+		$(".slider-area").owlCarousel({
+			items:1,
+			loop:true,
+			dots:false,
+			nav:true,
+			navText:["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"]
+			
+		})
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
