b25fea58ed79285a4d5c71df376f8be7df5e24be
monowar-hossain <32286368+monowar-hossain@users.noreply.github.com>
2017-10-25 16:03:45 +0600

    class-46

---
+++ class-46/assets/js/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".products-carosel").owlCarousel({
+			
+			items: 1,
+            autoplay: false,
+            loop: true,
+            nav: false,
+            dots: true,
+			
+			
+		});
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
