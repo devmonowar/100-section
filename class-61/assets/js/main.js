491d4723b812a99e372ca005298e1f1ebde8e14c
monowar-hossain <32286368+monowar-hossain@users.noreply.github.com>
2017-11-07 15:15:52 +0600

    class-61

---
+++ class-61/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+		
+			var center = [23.90317,89.1288885];
+			$('.map').gmap3({
+						center: center,
+						scrollwheel: false,
+						zoom: 10
+					  })
+					  .marker({
+						position: center,
+						icon: 'http://monowar.tk/class-60/assets/img/map-marker.png'
+					  })
+					
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
