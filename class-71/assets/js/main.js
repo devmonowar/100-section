ac242714964ec75c6a77d977ea5d75d793c65e9f
monowar-hossain <32286368+monowar-hossain@users.noreply.github.com>
2017-11-13 16:39:04 +0600

    class-71

---
+++ class-71/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+		$('[data-toggle="popover"]').popover();
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+		
+		 $('#myModal').modal()
+        
+    });
+
+
+}(jQuery));	
