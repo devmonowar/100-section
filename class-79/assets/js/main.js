0a8eaa934fddb2b387d6941a4d04e7639911e3e8
monowar-hossain <32286368+monowar-hossain@users.noreply.github.com>
2017-11-19 23:55:27 +0600

    class-79

---
+++ class-79/assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $('.mailchimp-form').ajaxChimp({
+			url: 'https://monowar.us17.list-manage.com/subscribe/post?u=31fa96956e07f3e784f03e98e&amp;id=3b837970b6'
+		});
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
