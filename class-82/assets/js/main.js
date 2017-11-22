185e699d5507a1c469a1ef32a0c271bc36906566
monowar-hossain <32286368+monowar-hossain@users.noreply.github.com>
2017-11-22 18:57:15 +0600

    class-83

---
+++ class-82/assets/js/main.js
@@ -0,0 +1,18 @@
+(function ($) {
+    "use strict";
+
+
+      $(document).ready(function(){
+        $(".feature-projects").owlCarousel({
+          items:1,
+          autolay:false,
+          nav:false,
+          dots:true,
+          loop:true  
+        });
+      });
+
+
+
+
+}(jQuery));
