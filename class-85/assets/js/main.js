a5f6ba17d8ece6fbdb31731079fd8a86aa19b996
monowar-hossain <32286368+monowar-hossain@users.noreply.github.com>
2017-11-24 15:29:35 +0600

    class-85

---
+++ class-85/assets/js/main.js
@@ -0,0 +1,16 @@
+
+    jQuery(document).ready(function($){
+
+
+    	$(".testimonial-carousel").owlCarousel({
+    		items: 1,
+    		dots: false,
+    		nav: true,
+    		navText: ['<i class="fa fa-long-arrow-left"></i>', '<i class="fa fa-long-arrow-right"></i>'],
+    		autoplay: false,
+			loop: true
+    	})
+
+
+
+    });
